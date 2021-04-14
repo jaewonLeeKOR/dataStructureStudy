bf8ef04da8be6fa8960c2610aad0a42807caefa7
jaewon <ghfkddl706@me.com>
2021-04-14 11:54:22 +0900

    start implementation nodeBase queue second time

---
+++ nodeBase_queue_second.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>class Queue;
+
+template<typename T>
+class Node{
+    int memberElement;
+    Node<T> *next;
+    Node<T>(T element):memberElement(element),next(nullptr) {}
+    friend class Queue<T>;
+};
+
+template<typename T>
+class Queue{
+    int queueSize;
+    Node<T> *frontNode;
+    Node<T> *rearNode;
+public:
+    Queue<T>():queueSize(0),frontNode(nullptr),rearNode(nullptr) {}
+    inline const int size() const {return queueSize;}
+    inline const bool empty() const {return queueSize==0;}
+    inline const T front() const {return frontNode->memberElement;}
+    void push(T);
+    T pop();
+    void print();
+};
+
+int main() {}
