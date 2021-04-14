37944a77af3e9044ddbcb4850614fd384583dadc
jaewon <ghfkddl706@me.com>
2021-04-14 17:22:23 +0900

    implement main function to test code

--- arrayBase_queue.cpp
+++ arrayBase_queue.cpp
@@ -15,11 +15,11 @@ public:
         for(int i=0;i<queueCapacity;i++)
             queue[i] = (T)NULL;
     }
-    inline const T rear() const {return queue[rearIndex];}
-    inline const int capacity() const {return queueCapacity;}
+    inline const T rear() const {return queue[rearIndex-1];}
+    inline const int capacity() const {return queueCapacity-1;}
     inline const int size() const {return (rearIndex - frontIndex + queueCapacity) % queueCapacity;}
     inline const bool empty() const {return size()==0;}
-    inline const bool full() const {return size()==capacity()-1;}
+    inline const bool full() const {return size()==capacity();}
     void enqueue(T);
     T dequeue();
     void print() const;
@@ -57,7 +57,7 @@ void Queue<T>::print() const
     if(empty())
     {
         cout << "Queue is Empty.\n";
-        return (T)NULL;
+        return ;
     }
     int tmp = frontIndex;
     while(tmp!=rearIndex)
@@ -68,4 +68,59 @@ void Queue<T>::print() const
     cout << "\n";
 }
 
-int main() {}
+int main()
+{
+    int testCase, size;
+    cin >> testCase;
+    cout << "queue size : ";
+    cin >> size;
+    Queue<int> queue(size);
+    string s;
+    for(int i=0;i<testCase;i++)
+    {
+        cin >> s;
+        if(s=="size")
+        {
+            cout << "Size : " << queue.size() << "\n";
+        }
+        else if(s=="capacity")
+        {
+            cout << "Capacity : " << queue.capacity() << "\n";
+        }
+        else if(s=="empty")
+        {
+            if(queue.empty())
+                cout << "Queue is empty!\n";
+            else
+                cout << "Queue is not empty\n";
+        }
+        else if(s=="enqueue")
+        {
+            int e;
+            cin >> e;
+            queue.enqueue(e);
+        }
+        else if(s=="dequeue")
+        {
+            if(queue.empty())
+                queue.dequeue();
+            else
+                cout << queue.dequeue() << "\n";
+        }
+        else if(s=="rear")
+        {
+            if(queue.rear()==(int)NULL)
+                cout << "Queue is empty.\n";
+            else
+                cout << queue.rear() << "\n";
+        }
+        else if(s=="print")
+        {
+            queue.print();
+        }
+        else if(s=="q")
+        {
+            break;
+        }
+    }
+}
