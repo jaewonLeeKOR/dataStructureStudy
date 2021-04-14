4ccf2e17e39df62224f3917b58ad02379a7c2110
jaewon <ghfkddl706@me.com>
2021-04-14 16:21:38 +0900

    implement size function

--- arrayBase_stack.cpp
+++ arrayBase_stack.cpp
@@ -14,6 +14,7 @@ public:
         for(int i=0;i<capacity;i++)
             stack[i] = (T)NULL;
     }
+    inline const int size() const {return stackSize;}
     inline const bool empty() const {return stackSize==0;}
     inline const T top() const {return empty()?(T)NULL:stack[stackSize-1];}
     void push(T);
