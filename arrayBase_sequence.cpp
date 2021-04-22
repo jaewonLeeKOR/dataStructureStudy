b1955b9bbd0e6dc1e42ca668e7444ec230772cba
jaewon <ghfkddl706@me.com>
2021-04-22 17:13:15 +0900

    start implement array based sequence

---
+++ arrayBase_sequence.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+template<typename T>
+class Sequence
+{
+    int sequenceSize;
+    int frontIndex;
+    int rearIndex;
+    T *sequence;
+    public:
+    Sequence(int s=10):sequenceSize(s+1),frontIndex(0),rearIndex(0)
+    {
+        sequence = new T[sequenceSize];
+        for(int i=0;i<sequenceSize;i++)
+        {
+            sequence[i] = (T)NULL;
+        }
+    }
+    const int size() const;
+    const bool empty() const;
+    const T front() const;
+    const T rear() const;
+    void insert(int index, int value);
+    void insertFront(int value);
+    void insertRear(int value);
+    T erase(int index);
+    T eraseFront();
+    T eraseRear();
+    void clear();
+    T at(int index);
+    T operator[](int index) const;
+    void print();
+    void printReverase();
+};
