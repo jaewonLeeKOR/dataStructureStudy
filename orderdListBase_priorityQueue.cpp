7e95d89381a15df0a0826f17f359ab84b78d3965
jaewon <ghfkddl706@me.com>
2021-05-29 02:57:20 +0900

    Declare PriorityQueue
    Declare orderd list based PriorityQueue

---
+++ orderdListBase_priorityQueue.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#define asc 1
+#define desc -1
+using namespace std;
+template<typename T>
+struct Entry {
+  int key;
+  T value;
+  Entry *next;
+  Entry *prev;
+  Entry():key(0),value(NULL),next(nullptr),prev(nullptr) {}
+  Entry(int k, T v):key(k),value(v),next(nullptr),prev(nullptr) {}
+};
+template<typename T>
+class PriorityQueue {
+  int priority;
+  Entry *header;
+  Entry *trailer;
+public:
+  PriorityQueue(int p):priority(p) {
+    header = new Entry();
+    trailer = new Entry();
+    header->next = trailer;
+    trailer->prev = header;
+  }
+  void insert(int key, T value); //key값과 value값을 가지는 entry를 우선순위 큐 내에 삽입한다
+  T removePriority(); //우선순위 큐의 우선순위가 가장높은 entry의 value를 리턴하고 제거한다
+  int priorityKey(); //우선순위 큐의 우선순위가 가정높은 entry의 key값을 리턴한다
+  T priorityValue(); //우선순위 큐의 우선순위가 가정높은 entry의 value값을 리턴한다
+  T rank(int rank); //우선순위 큐의 우선순위가 rank번째인 entry의 value를 리턴한다
+  int size(); //우선순위 큐의 entry 갯수를 리턴한다.
+  bool empty(); //우선순위 큐가 비어있는지 리턴한다.
+};
