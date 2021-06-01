c77f273dbc239450fa13e868c4c9ee7787487389
jaewon <ghfkddl706@me.com>
2021-06-01 21:33:14 +0900

    implement removePriority function

--- orderdListBase_priorityQueue.cpp
+++ orderdListBase_priorityQueue.cpp
@@ -25,7 +25,7 @@ public:
   }
   void insert(int key, T value); //key값과 value값을 가지는 entry를 우선순위 큐 내에 삽입한다
   T removePriority(); //우선순위 큐의 우선순위가 가장높은 entry의 value를 리턴하고 제거한다
-  int priorityKey(); //우선순위 큐의 우선순위가 가정높은 entry의 key값을 리턴한다
+  int priorityKey(); //우선순위 큐의 우선순위가 가장높은 entry의 key값을 리턴한다
   T priorityValue(); //우선순위 큐의 우선순위가 가정높은 entry의 value값을 리턴한다
   T rank(int rank); //우선순위 큐의 우선순위가 rank번째인 entry의 value를 리턴한다
   int size(); //우선순위 큐의 entry 갯수를 리턴한다.
@@ -35,8 +35,8 @@ public:
 };
 template<typename T>
 void PriorityQueue<T>::insert(int key, T value) {
-  Entry<T> newEntry = new Entry<T>(key,value);
-  Entry<T> tmp=header->next;
+  Entry<T> *newEntry = new Entry<T>(key,value);
+  Entry<T> *tmp=header->next;
   for(; tmp != trailer; tmp = tmp->next) {
     if(key > tmp->key)
       continue;
@@ -47,4 +47,15 @@ void PriorityQueue<T>::insert(int key, T value) {
   newEntry->prev = tmp->prev;
   tmp->prev = newEntry;
   newEntry->next = tmp;
+}
+template<typename T>
+T PriorityQueue<T>::removePriority() {
+  T res = rank(1);
+  if(size() == 0)
+    return NULL;
+  Entry<T> *tmp = header->next;
+  header->next = tmp->next;
+  header->next->prev = header;
+  delete tmp;
+  return res;
 }
